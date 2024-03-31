dafe4615da230a5b6349fb7e33ca2f70a02dc52d
Amangeldy Tomiris <70026328+Tomiris-Aman@users.noreply.github.com>
2024-03-31 23:32:54 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// HOBBIES 
 const originalText = document.getElementById('home-text');
 const alternateTextBook = document.getElementById('alternateTextBook');
 const alternateTextScience = document.getElementById('alternateTextScience');
@@ -6,6 +7,7 @@ const alternateTextGym = document.getElementById('alternateTextGym');
 const alternateTextPen = document.getElementById('alternateTextPen');
 const alternateTextNail = document.getElementById('alternateTextNail');
 const alternateTextCode = document.getElementById('alternateTextCode');
+const alternateTextTalk = document.getElementById('alternateTextTalk');
 function readingText() {
     originalText.style.display = 'none';
     alternateTextScience.style.display = 'none';
@@ -13,8 +15,20 @@ function readingText() {
     alternateTextGym.style.display = 'none';
     alternateTextPen.style.display = 'none';
     alternateTextNail.style.display = 'none';
+    alternateTextTalk.style.display = 'none';
     alternateTextCode.style.display = 'none';
     alternateTextBook.style.display = 'block';
+}
+function talkText() {
+    originalText.style.display = 'none';
+    alternateTextScience.style.display = 'none';
+    alternateTextAstro.style.display = 'none';
+    alternateTextGym.style.display = 'none';
+    alternateTextPen.style.display = 'none';
+    alternateTextNail.style.display = 'none';
+    alternateTextCode.style.display = 'none';
+    alternateTextBook.style.display = 'none';
+    alternateTextTalk.style.display = 'block';
 }
 function scienceText() {
     originalText.style.display = 'none';
@@ -24,6 +38,7 @@ function scienceText() {
     alternateTextPen.style.display = 'none';
     alternateTextNail.style.display = 'none';
     alternateTextCode.style.display = 'none';
+    alternateTextTalk.style.display = 'none';
     alternateTextScience.style.display = 'block';
 }
 function astroText() {
@@ -33,6 +48,7 @@ function astroText() {
     alternateTextGym.style.display = 'none';
     alternateTextPen.style.display = 'none';
     alternateTextNail.style.display = 'none';
+    alternateTextTalk.style.display = 'none';
     alternateTextCode.style.display = 'none';
     alternateTextAstro.style.display = 'block';
 }
@@ -42,6 +58,7 @@ function gymText() {
     alternateTextAstro.style.display = 'none';
     alternateTextBook.style.display = 'none';
     alternateTextPen.style.display = 'none';
+    alternateTextTalk.style.display = 'none';
     alternateTextNail.style.display = 'none';
     alternateTextCode.style.display = 'none';
     alternateTextGym.style.display = 'block';
@@ -52,6 +69,7 @@ function writingText() {
     alternateTextAstro.style.display = 'none';
     alternateTextGym.style.display = 'none';
     alternateTextBook.style.display = 'none';
+    alternateTextTalk.style.display = 'none';
     alternateTextNail.style.display = 'none';
     alternateTextCode.style.display = 'none';
     alternateTextPen.style.display = 'block';
@@ -62,6 +80,7 @@ function nailText() {
     alternateTextAstro.style.display = 'none';
     alternateTextGym.style.display = 'none';
     alternateTextPen.style.display = 'none';
+    alternateTextTalk.style.display = 'none';
     alternateTextBook.style.display = 'none';
     alternateTextCode.style.display = 'none';
     alternateTextNail.style.display = 'block';
@@ -71,6 +90,7 @@ function codingText() {
     alternateTextScience.style.display = 'none';
     alternateTextAstro.style.display = 'none';
     alternateTextGym.style.display = 'none';
+    alternateTextTalk.style.display = 'none';
     alternateTextPen.style.display = 'none';
     alternateTextNail.style.display = 'none';
     alternateTextBook.style.display = 'none';
